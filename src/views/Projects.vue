a083cf9402b00581f407d9ce8b5aa99e783431a4
Anthony Aiello <aiell016@rangers.uwp.edu>
2019-06-27 00:19:20 -0500

    Good enough to publish. Needs contact page and more projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,6 +2,77 @@
     <div class="about">
         <h1>Projects</h1>
 
+
+
+        <div class="centercontent">
+            <h4>Non Profit Veterans Organization</h4>
+        </div>
+
+        <div class="centercontent">
+            <h4>Mission Got Your Six</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The Mission Got Your Six project is a website project for a local, non-profit, veteran's organization.
+                Their slogan is "Never Alone,  22", is one that they hope will raise awareness of the fact that 22 veterans commit suicide each year, and this
+                mission's goal is to provide medical and other services free of charge to first responders and Unites States Veterans.
+                The organization offers networking opportunities by hosting activites, softball games, and social events. Fundraising and donations are important components, and are 
+                greatly appreciated and needed in order to offer this support for our Heroes.
+                <div >
+                    <img  class="contain" src="../assets/six1.png" style="width:1582px;height:735px;">
+                </div>
+            <br /> 
+            <hr />
+            <div>
+                <img  class="contain" src="../assets/six2.png" style="width:1582px;height:735px;">
+            </div>
+            <br />
+        </div>
+        <hr />
+        <br />
+        <br />
+
+
+
+
+
+
+
+        <div class="centercontent">
+            <h4>Conference Website</h4>
+        </div>
+        <div class="centercontent">
+            <h4>Consortium For Computing Sciences in Colleges Website 2019</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This conference website design and build was assigned to me during the early spring of 2018 by one of our
+                Computer Science professors, Dr. Zaid Altahat.  I've continued the project for 2 years and designed the current 2019 conference. 
+                This 2019 website covers one of several of these conferences that occur frequently in various regions of the United States.
+                The conference offers workshops, classes, discussions, panels, and other activities.  The website offers guides, registration,
+                maps, and information about the conference. This conference also gives scholars a chance for submitting a paper for conference publication in IEEE,
+                The Institute of Electrical and Electronic Engineers, (sometimes just called I-Triple-E).
+            </p>
+                <div >
+                    <img  class="contain" src="../assets/ccscmw2019.png" style="width:1580px;height:638px;">
+                </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
+
+
+
+        <div class="centercontent">
+            <h4>VueJS Application</h4>
+        </div>
         <div class="centercontent">
             <h4>On The Radar</h4>
         </div>
@@ -18,10 +89,6 @@
             <p>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The next team to take over this project rewrote the app using Angular.
             </p>
-
-
-
-
                 <div class="floatright">
                     <img  class="scale-down" src="../assets/otr3.png" style="width:500px;height:400px;">
                 </div>
@@ -42,21 +109,194 @@
                 </div>
                 <br /> 
         </div>
+        <hr />
         <br />
         <br />
-
-
+        <div class="centercontent">
+            <h4>E-Commerce</h4>
+        </div>
         <div class="centercontent">
             <h4>Project Volga</h4>
         </div>
         <br />
         <br />
 
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Project Volga was the first site I designed after Computer Science 1. 
+                This project came from Web Concepts I and II.  Volga is a river, like the Nile River, or the Amazon River.
+                Well, this is project Volga, and it functions like the other e-commerce rivers, kind of.  This site focused
+                on the front end for the first semester, and the back end for the second semester.  Project Volga was one
+                of the most fun and challenging projects to complete. This is before I’ve had design classes.
+                I’m just saying. Click on the screen shots to go into that website.  
+
+            </p>
+            <div class="floatnone">
+                <img  class="contain" src="../assets/volga1.png" style="width:1068px;height:765px;">
+            </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
+
+        <div class="centercontent">
+            <h4>Business Website</h4>
+        </div>
+
+        <div class="centercontent">
+            <h4>App Factory Website</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The App Factory website started off as an undergrad
+                 Software Engineering 1 and 2 project. The website is built using WordPress on an
+                 AWS LAMP stack in an EC2 instance. In other words, this site is hosted on
+                 Amazon Web Services (AWS) and is built using WordPress. The site features screenshots
+                 of some of the apps built by student intern programmers and designers in the App Factory, 
+                 an on-campus business that develops applications for regional businesses and organizations. 
+            </p>
+                <div >
+                    <img  class="contain" src="../assets/appfactory.png" style="width:1586px;height:775px;">
+                </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
+
+        <div class="centercontent">
+            <h4>Conference Website</h4>
+        </div>
+        <div class="centercontent">
+            <h4>Consortium For Computing Sciences in Colleges Website 2018</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This conference website design and build was tasked to me during the early spring of 2018.
+                The website covers one of several of these conferences that occur frequently in various regions of the United States.
+                The conference offers workshops, classes, discussions, panels, and other activities.  The website offers guides, registration,
+                maps, and information about the conference. This conference also gives scholars a chance for submitting a paper for conference publication in IEEE,
+                The Institute of Electrical and Electronic Engineers, (sometimes just called I-Triple-E).
+            </p>
+                <div >
+                    <img  class="contain" src="../assets/ccscmw2018.png" style="width:1582px;height:739px;">
+                </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
+
+        <div class="centercontent">
+            <h4>Blog</h4>
+        </div>
+
+        <div class="centercontent">
+            <h4>Gear Blabs Website</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The Gear Blabs website was a homework project for Art 477 where we had to create our first WordPress blog.
+                Images of sound and lighting for bands and shows fill the pages of this mockup blog, that had to be created to specifications. 
+            </p>
+                <div >
+                    <img  class="contain" src="../assets/gear.png" style="width:1905px;height:923px;">
+                </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
 
 
 
 
-    </div>
+
+        <div class="centercontent">
+            <h4>E-Commerce</h4>
+        </div>
+
+        <div class="centercontent">
+            <h4>Rainbow of Dreams Gifts</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rainbow of Dreams Gifts is a gift and craft fair shows business based in Racine, Wisconsin.
+                The owners, Pat and Patrick Shay sell boutique goods and unique items that are found at one of the many shows they attend.
+                Rainbow of Dreams website is a WordPress based content management system (CMS).  A WooCommerce e-commerce package is also
+                installed to facilitate online shoppping through the website store front.
+            </p>
+                <div >
+                    <img  class="contain" src="../assets/rainbow.png" style="width:1581px;height:773px;">
+                </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
+
+
+
+
+
+
+
+        <div class="centercontent">
+            <h4>Event Website</h4>
+        </div>
+
+        <div class="centercontent">
+            <h4>Chi Steppin</h4>
+        </div>
+        <br />
+        <br />
+
+        <div>
+            <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chi Steppin is a style of unique dance popular in this area, which is 90 minutes north of Chicago.
+                This is an event produced by private individuals, and offers dance classes and lessons from professionals.  The weekend event is 
+                private, and advanced ticket purchase is suggested.  
+            </p>
+                <div >
+                    <img  class="contain" src="../assets/chi.png" style="width:1582px;height:749px;">
+                </div>
+            <br /> 
+        </div>
+        <hr />
+        <br />
+        <br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
 </template>
 
 <style scoped>
