8dd197d95ad5810b217178522663fb33f13601de
Anthony Aiello <aiell016@rangers.uwp.edu>
2019-06-27 18:11:44 -0500

    Added contact phone button

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,7 +2,16 @@
   <div class="contact">
     <h1>Contact</h1>
 
+<div>
+               <h4>Contact me</h4>
+               <br />
 
+            <a href="tel:2626710703"><img src = "../assets/call.png"></a>
+            <br />
+            <p>You may use this to contact me.  Please leave a message.
+              </p>
+
+             </div>
     
   </div>
 </template>
