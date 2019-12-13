7e018d9185ac58079a78379bbaeae7fa2c4cf62a
Tony Aiello <aielloa@uwp.edu>
2019-12-13 13:13:21 -0600

    Added comments to Readme file

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
-# TonyAielloio
+# Tony@Aiello.io
+
+## Before running:
+
+    *  There is no user login in the front end yet.
+    *  
+    * 
+    *
+    *  In the file located at:  components > http.js the access token will need to be updated.   
+    *  Copy and paste the new token from Postman response in the x-access-token value.
+    *  Instructions are also in the comments section in that file.
+    *
+    * 
+    *  To Update this access token manually, use postman with the following credentials in the Body:
+    *
+    *
+    *  Test user
+    * 
+    *    {  "email": "temp@test.com",
+    *       "password": "1234"
+    *    }
+    *
+    * 
+    * Server is hosted at:
+    * 
+    *     https://server-cli-api.herokuapp.com/api/users/login
+    *
+    *
+    * Admin user:
+    *   {  "email": "s@s.com",
+    *      "password": "1234"
+    *   }
+    *   
+    * 
+    * 
+
 
 ## Project setup
 ```
 npm install
 ```
+## Dev
+npm run dev
 
 ### Compiles and hot-reloads for development
 ```
@@ -27,3 +64,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
--- src/components/http.js
+++ src/components/http.js
@@ -4,10 +4,38 @@ export const http = axios.create({
  
   baseURL: 'https://server-cli-api.herokuapp.com/api/',
   headers: { 
-    'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkZGM0ZjRmNTNkMzZiZjhiYjNkODA1ZiIsImlhdCI6MTU3NjE5NTg0NywiZXhwIjoxNTc2MjgyMjQ3fQ.p-9oCL3UQp4GcNmEAMup7J7E-aNBxpqw7b-xX0Q2wDQ'
-    /*'X-API-KEY': '05b6acf0e04a9dce45f178cb10b51771',
-    'Access-Control-Allow-Origin': "*",
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'*/
+    'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkZGM0ZjRmNTNkMzZiZjhiYjNkODA1ZiIsImlhdCI6MTU3NjI2NDEyMywiZXhwIjoxNTc2MzUwNTIzfQ.zaZTaRR3Kc3WZQB4h86YSlPuJ3vvXah9f_hdg8c9LbY'
+
+    /*   
+    *  The x-acces token above needs to be renewed if over 2 hours.
+    *  There is no user login in the front end yet.
+    * 
+    *  Copy and paste the new token from Postman response in the x-access-token value in the line above this.
+    * 
+    *  To Update this access token manually, use postman with the following credentials in the Body:
+    *
+    *
+    *  Test user
+    * 
+    *    {  "email": "temp@test.com",
+    *       "password": "1234"
+    *    }
+    *
+    * 
+    * Server is hosted at:
+    * 
+    *     https://server-cli-api.herokuapp.com/api/users/login
+    *
+    *
+    * Admin user:
+    *   {  "email": "s@s.com",
+    *      "password": "1234"
+    *   }
+    *   
+    * 
+    * 
+    */
+
   }
 
   
